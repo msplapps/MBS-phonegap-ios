937b0107f175d996b4790542f6451dda7c0e4b02
Toby K <toby@codeworksltd.co.uk>
2017-10-27 19:27:45 +0530

    logout test line: 19811

--- build/mm.bundle.js
+++ build/mm.bundle.js
@@ -19808,7 +19808,7 @@ angular.module('mm.core.login')
                 disableAnimate: true,
                 disableBack: true
             });
-            $state.go('mm_login.sites');
+            $state.go('mm_login.credentials');
         });
     };
     $scope.login = function() {
